d0d6c60ec3086109a992efa9e0baa1d351983b95
MuhammadSaidulislam <mislam171222@bscse.uiu.ac.bd>
2025-10-27 11:40:18 +0600

    cv download add

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const CV = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
     return (
         <div className='cvSection'>
             <div className='container'>
@@ -8,11 +13,14 @@ const CV = () => {
                     <h1>Atif Ellahie</h1>
                     <button>CV Download</button>
                 </div>
-                <iFrame
-                    src="/Atif-Ellahie-CV.pdf"
-                    width="100%"
-                    height="800px"
-                />
+                <div style={{ height: '800px', border: '1px solid #ddd', borderRadius: '8px' }}>
+                    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
+                        <Viewer
+                            fileUrl="/Atif-Ellahie-CV.pdf"
+                            plugins={[defaultLayoutPluginInstance]}
+                        />
+                    </Worker>
+                </div>
             </div>
         </div>
     )
